c3a672798924a0aa66e9bcc259909572f49b506a
AJTJ <janke.aaron@gmail.com>
2023-04-01 13:28:40 -0600

    update Sat  1 Apr 2023 13:28:40 CST

--- src/heap_sort.rs
+++ src/heap_sort.rs
@@ -1,3 +1,10 @@
+/*
+- It is simpler to use rust's built-in methods
+- It could be written like so:
+    - https://www.hackertouch.com/heap-sort-in-rust.html
+
+ */
+
 use std::collections::BinaryHeap;
 
 pub fn heap_sort(list: &[i32]) -> Vec<i32> {
@@ -11,6 +18,7 @@ pub fn heap_sort(list: &[i32]) -> Vec<i32> {
     create_heap(list).into_sorted_vec()
 }
 
+// this could be used, but it's a bit more manual than necessary
 pub fn create_heap(list: &[i32]) -> BinaryHeap<i32> {
     let mut heap = BinaryHeap::new();
     for x in list {
@@ -19,7 +27,7 @@ pub fn create_heap(list: &[i32]) -> BinaryHeap<i32> {
     heap
 }
 
-// NOTE: this is the in-place version
+// NOTE: this is the in-place version, if needed
 // fn create_max_heap(data: &mut Vec<i32>, length: usize, index: usize) {
 //     let mut largest = index;
 //     let mut left = 2 * index + 1;
